e5b5457f13c4c1ebdb143bfa0b4c1ae86659b4fc
Thiago Moreira Bianeck <thiagobianeck@gmail.com>
2020-12-01 00:23:52 -0300

    24 - Sobre a especificação JPA

---
+++ src/main/java/com/bianeck/minhasfinancas/model/entity/StatusLancamento.java
@@ -0,0 +1,8 @@
+package com.bianeck.minhasfinancas.model.entity;
+
+public enum StatusLancamento {
+
+    PENDENTE,
+    CANCELADO,
+    EFETIVADO
+}
---
+++ src/main/java/com/bianeck/minhasfinancas/model/entity/TipoLancamento.java
@@ -0,0 +1,7 @@
+package com.bianeck.minhasfinancas.model.entity;
+
+public enum TipoLancamento {
+
+    RECEITA,
+    DESPESA
+}
