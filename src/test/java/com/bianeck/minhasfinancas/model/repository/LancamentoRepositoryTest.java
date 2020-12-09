f4e3ca0930167d3bc6ea0ccffe48901b052433e6
Thiago Moreira Bianeck <thiagobianeck@gmail.com>
2020-12-08 21:29:36 -0300

    86. Testando a deleção de lançamentos na camada de serviç

--- src/test/java/com/bianeck/minhasfinancas/model/repository/LancamentoRepositoryTest.java
+++ src/test/java/com/bianeck/minhasfinancas/model/repository/LancamentoRepositoryTest.java
@@ -1,5 +1,10 @@
 package com.bianeck.minhasfinancas.model.repository;
 
+import com.bianeck.minhasfinancas.model.entity.Lancamento;
+import com.bianeck.minhasfinancas.model.entity.Usuario;
+import com.bianeck.minhasfinancas.model.enums.StatusLancamento;
+import com.bianeck.minhasfinancas.model.enums.TipoLancamento;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,6 +14,12 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
@@ -23,9 +34,85 @@ public class LancamentoRepositoryTest {
     LancamentoRepository repository;
 
     @Test
-    @DisplayName("")
-    public void a() {
+    @DisplayName("Deve salvar um lançamento.")
+    public void deveSalvarUmLancamento() {
 
+        Lancamento lancamento = criarLancamento();
+
+        lancamento = repository.save(lancamento);
+
+        assertThat(lancamento.getId()).isNotNull();
+
+    }
+
+    @Test
+    @DisplayName("Deve deletar um lancamento")
+    public void deveDeletarUmLancamento() {
+        Lancamento lancamento = criarEPersistirUmLancamento();
+
+        lancamento = entityManager.find(Lancamento.class, lancamento.getId());
+
+        repository.delete(lancamento);
+
+        Lancamento lancamentoInexistente = entityManager.find(Lancamento.class, lancamento.getId());
+
+        assertThat(lancamentoInexistente).isNull();
+
+    }
+
+
+
+    @Test
+    @DisplayName("Deve atualizar um lançamento.")
+    public void deveAtualizarUmLancamento() {
+        Lancamento lancamento = criarEPersistirUmLancamento();
+
+        int ano = 2018;
+        String descricao = "Teste Atualizar";
+
+        lancamento.setAno(ano);
+        lancamento.setDescricao(descricao);
+        lancamento.setStatus(StatusLancamento.CANCELADO);
+
+        repository.save(lancamento);
+
+        Lancamento lancamentoAtualizado = entityManager.find(Lancamento.class, lancamento.getId());
+
+        assertThat(lancamentoAtualizado.getAno()).isEqualTo(ano);
+        assertThat(lancamentoAtualizado.getDescricao()).isEqualTo(descricao);
+        assertThat(lancamentoAtualizado.getStatus()).isEqualTo(StatusLancamento.CANCELADO);
+
+    }
+
+    @Test
+    @DisplayName("Deve buscar um lançamento por id.")
+    public void deveBuscarUmLancamentoPorId() {
+        Lancamento lancamento = criarEPersistirUmLancamento();
+
+        Optional<Lancamento> lancamentoEncontrado = repository.findById(lancamento.getId());
+
+        assertThat(lancamentoEncontrado.isPresent()).isTrue();
+    }
+
+    private Lancamento criarEPersistirUmLancamento() {
+        Lancamento lancamento = criarLancamento();
+
+        entityManager.persist(lancamento);
+        return lancamento;
+    }
+
+    public static Lancamento criarLancamento() {
+        Lancamento lancamento = Lancamento.builder()
+                .usuario(Usuario.builder().id(1L).build())
+                .ano(2019)
+                .mes(1)
+                .descricao("Lancamento Qualquer")
+                .valor(BigDecimal.TEN)
+                .tipo(TipoLancamento.RECEITA)
+                .status(StatusLancamento.PENDENTE)
+                .dataCadastro(LocalDate.now())
+                .build();
+        return lancamento;
     }
 
 
--- src/test/java/com/bianeck/minhasfinancas/service/LancamentoServiceTest.java
+++ src/test/java/com/bianeck/minhasfinancas/service/LancamentoServiceTest.java
@@ -1,15 +1,26 @@
 package com.bianeck.minhasfinancas.service;
 
+import com.bianeck.minhasfinancas.exceptions.RegraNegocioException;
+import com.bianeck.minhasfinancas.model.entity.Lancamento;
+import com.bianeck.minhasfinancas.model.entity.Usuario;
+import com.bianeck.minhasfinancas.model.enums.StatusLancamento;
 import com.bianeck.minhasfinancas.model.repository.LancamentoRepository;
+import com.bianeck.minhasfinancas.model.repository.LancamentoRepositoryTest;
 import com.bianeck.minhasfinancas.service.impl.LancamentoServiceImpl;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static com.bianeck.minhasfinancas.model.repository.LancamentoRepositoryTest.criarLancamento;
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
@@ -23,9 +34,80 @@ public class LancamentoServiceTest {
     LancamentoRepository repository;
 
     @Test
-    @DisplayName("")
-    public void a(){
+    @DisplayName("Deve salvar um Lançamento")
+    public void deveSalvarumLancamento(){
+        Lancamento lancamentoASalvar = criarLancamento();
+        Mockito.doNothing().when(service).validar(lancamentoASalvar);
 
+        Lancamento lancamentoSalvo = criarLancamento();
+        lancamentoSalvo.setId(1L);
+        lancamentoSalvo.setStatus(StatusLancamento.PENDENTE);
+        Mockito.when(repository.save(lancamentoASalvar)).thenReturn(lancamentoSalvo);
+
+        Lancamento lancamento = service.salvar(lancamentoASalvar);
+
+        assertThat(lancamento.getId()).isEqualTo(lancamentoSalvo.getId());
+        assertThat(lancamento.getStatus()).isEqualTo(StatusLancamento.PENDENTE);
+    }
+
+    @Test
+    @DisplayName("Não deve salvar um lançamento quando houver erro de validação")
+    public void naoDeveSalvarUmLancamentoQuandoHouverErroDeValidacao() {
+        Lancamento lancamentoASalvar = criarLancamento();
+        Mockito.doThrow(RegraNegocioException.class).when(service).validar(lancamentoASalvar);
+
+        catchThrowableOfType(() -> service.salvar(lancamentoASalvar), RegraNegocioException.class);
+
+        Mockito.verify(repository, Mockito.never()).save(lancamentoASalvar);
+    }
+
+    @Test
+    @DisplayName("Deve atualizar um Lançamento")
+    public void deveAtualizarumLancamento(){
+
+        Lancamento lancamentoSalvo = criarLancamento();
+        lancamentoSalvo.setId(1L);
+        lancamentoSalvo.setStatus(StatusLancamento.PENDENTE);
+
+        Mockito.doNothing().when(service).validar(lancamentoSalvo);
+        Mockito.when(repository.save(lancamentoSalvo)).thenReturn(lancamentoSalvo);
+
+        service.atualizar(lancamentoSalvo);
+
+        Mockito.verify(repository, Mockito.times(1)).save(lancamentoSalvo);
+
+    }
+
+    @Test
+    @DisplayName("Deve lançar erro ao tentar atualizar um lançamento que ainda não foi salvo.")
+    public void deveLancarErroAoTentarAtualizarUmLancamentoQueAindaNaoFoiSalvo() {
+        Lancamento lancamento = criarLancamento();
+
+        catchThrowableOfType(() -> service.atualizar(lancamento), NullPointerException.class);
+
+        Mockito.verify(repository, Mockito.never()).save(lancamento);
+    }
+
+    @Test
+    @DisplayName("Deve deletar um lançamento.")
+    public void deveDeletarUmLancamento() {
+        Lancamento lancamento = criarLancamento();
+        lancamento.setId(1L);
+
+        service.deletar(lancamento);
+
+        Mockito.verify(repository).delete(lancamento);
+
+    }
+
+    @Test
+    @DisplayName("Deve lançar erro ao tentar deletar um lançamento que ainda não foi salvo.")
+    public void deveLancarErroAoTentarDeletarUmLancamentoQueAindaNaoFoiSalvo() {
+        Lancamento lancamento = criarLancamento();
+
+        catchThrowableOfType(() -> service.deletar(lancamento), NullPointerException.class);
+
+        Mockito.verify(repository, Mockito.never()).delete(lancamento);
     }
 
 }
